5a62e10613a510f30077d89dbf5dc368b574ba3e
mcx <807317397@qq.com>
2019-01-31 15:07:33 +0800

    1

--- src/User/Client/Client.php
+++ src/User/Client/Client.php
@@ -1,27 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/3/17
- * Time: 22:47
- */
+
 
 namespace User\Client;
 
 
 class Client
 {
-    /**
-     * No explanation
-     *
-     * @author Zhiqiang Guo
-     * @return void
-     * @throws Exception
-     * @access public
-     */
+    
     public function index ()
     {
-        echo "this is client";
+        echo "this is client2";
     }
 
 }
